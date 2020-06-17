dfe0f9f714ee25aa30622f9dfdd67caf691f9343
rmylonas <roman.mylonas@isb-sib.ch>
2020-06-17 12:31:18 +0200

    correctly throw 404 errors

--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,34 @@ use Slim\Http\Response;
 // Routes
 
 // returns json object with all proteinAC data
+$app->get('/fasta/{protein}', function (Request $request, Response $response, $args) {
+    $arg = $request->getAttribute('protein');
+    $this->logger->addInfo("arg: " . $arg);
+
+    // OK for fetch proteinAC
+    try {
+        $mng = new MongoDB\Driver\Manager(\Src\Classes\Mongo\DB_AUTH_HOST);
+        $filter = [ 'ac' => $arg ];
+        $query = new MongoDB\Driver\Query($filter);
+        $res = $mng->executeQuery(\Src\Classes\Mongo\DB_NAME.".fasta", $query);
+        $prot = current($res->toArray());
+        if (!empty($prot)) {
+            return json_encode($prot);
+        } else {
+            echo "No match found\n";
+            throw new \Slim\Exception\NotFoundException($request, $response);
+        }
+    } catch (MongoDB\Driver\Exception\Exception $e) {
+        $filename = basename(__FILE__);
+        echo "The $filename script has experienced an error.\n";
+        echo "It failed with the following exception:\n";
+        echo "Exception:", $e->getMessage(), "\n";
+        echo "In file:", $e->getFile(), "\n";
+        echo "On line:", $e->getLine(), "\n";
+    }
+});
+
+// returns json object with all proteinAC data
 $app->get('/protein/{protein}', function (Request $request, Response $response, $args) {
     $arg = $request->getAttribute('protein');
     $this->logger->addInfo("arg: " . $arg);
@@ -15,12 +43,13 @@ $app->get('/protein/{protein}', function (Request $request, Response $response, 
         $mng = new MongoDB\Driver\Manager(\Src\Classes\Mongo\DB_AUTH_HOST);
         $filter = [ 'alternativeProteinACs' => $arg ];
         $query = new MongoDB\Driver\Query($filter);
-        $res = $mng->executeQuery(\Src\Classes\Mongo\DB_NAME.".".\Src\Classes\Mongo\DB_COLLECTION, $query);
+        $res = $mng->executeQuery(\Src\Classes\Mongo\DB_NAME.".proteins", $query);
         $prot = current($res->toArray());
         if (!empty($prot)) {
             return json_encode($prot);
         } else {
             echo "No match found\n";
+            throw new \Slim\Exception\NotFoundException($request, $response);
         }
     } catch (MongoDB\Driver\Exception\Exception $e) {
         $filename = basename(__FILE__);
@@ -43,7 +72,7 @@ $app->get('/query/{query}', function (Request $request, Response $response, $arg
         $filter = ['searchTerm' => new MongoDB\BSON\Regex( "$arg", 'i' )];
         $options = ['limit' => 100];
         $query = new MongoDB\Driver\Query($filter,$options);
-        $res = $mng->executeQuery(\Src\Classes\Mongo\DB_NAME.".".\Src\Classes\Mongo\SEARCH_COLLECTION, $query);
+        $res = $mng->executeQuery(\Src\Classes\Mongo\DB_NAME.".searches", $query);
         if (!empty($res)) {
             $output = array();
             foreach ($res as $row) {
