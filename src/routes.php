50398d31a17d1f856c0c39e5a9e71ccddf3da7f0
rmylonas <roman.mylonas@isb-sib.ch>
2020-07-08 14:18:47 +0200

    search descriptions for given proteinACs

--- src/routes.php
+++ src/routes.php
@@ -95,3 +95,35 @@ $app->get('/query/{query}', function (Request $request, Response $response, $arg
     }
 });
 
+
+
+// returns json object with list of fastaHeaders matches
+$app->get('/descriptions/{proteinACs}', function (Request $request, Response $response, $args) {
+    $arg = $request->getAttribute('proteinACs');
+    $this->logger->addInfo("arg: " . $arg);
+    $protein_acs = explode(",", $arg);
+
+    // search with partial matches
+    try {
+        $collection = (new MongoDB\Client(\Src\Classes\Mongo\DB_AUTH_HOST))->selectDatabase(\Src\Classes\Mongo\DB_NAME)->fasta;
+        $query = ['ac' => ['$in' => $protein_acs]];
+        $projection = ['projection' => ['gene_name' => true, '_id' => false, 'protein_name' => true]];
+        $cursor = $collection->find($query, $projection);
+        $descs = $cursor->toArray();
+
+        if (!empty($descs)) {
+            print_r(json_encode($descs));
+        } else {
+            echo "No match found\n";
+        }
+    } catch (MongoDB\Driver\Exception\Exception $e) {
+        $filename = basename(__FILE__);
+        echo "The $filename script has experienced an error.\n";
+        echo "It failed with the following exception:\n";
+        echo "Exception:", $e->getMessage(), "\n";
+        echo "In file:", $e->getFile(), "\n";
+        echo "On line:", $e->getLine(), "\n";
+    }
+});
+
+
